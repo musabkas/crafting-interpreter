6d4513dabae44b4acc9daf9f5c84559ec1bfb455
Musab Kasbati <musab16000@gmail.com>
2026-05-27 15:26:16 +0500

    base script and prompt interface

--- loxi.cpp
+++ loxi.cpp
@@ -1,14 +1,49 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+
+void run(std::string source) {
+    for (char letter : source) {
+        std::cout << letter;
+    }
+    std::cout << std::endl;
+}
+
+void runPrompt() {
+    while (true) {
+        std::cout << "> ";
+        std::string line;
+        if (!std::getline(std::cin, line)) break; // if EOF sent then end program, else read line
+        run(line);
+    }
+    std::cout << std::endl;
+}
+
+void runScript(std::string path){
+    std::ifstream script(path);
+    if (!script.is_open()) {
+        std::cout << "Could not open file: " << path << std::endl;
+        return;
+    }
+
+    std::string source;
+    std::string line;
+    while (std::getline(script, line)) {
+        source += line + "\n";
+    }
+    source.pop_back(); // remove final \n
+    run(source);
+}
+
 
 int main(int argc, char* argv[]){
     if (argc > 2) {
         std::cout << "Usage: loxi [script]" << std::endl;
     } else if (argc == 2) {
-        std::cout << "running script " << argv[1] << std::endl;
-        // runFile(argv[0]);
+        std::cout << "Running script " << argv[1] << std::endl;
+        runScript(argv[1]);
     } else {
-        std::cout << "running prompt " << std::endl;
-        // runPrompt();
+        std::cout << "Running prompt " << std::endl;
+        runPrompt();
     }
 }
--- makefile
+++ makefile
@@ -1,2 +1,10 @@
 build:
 	g++ loxi.cpp -o loxi.out
+
+runprompt:
+	make build
+	./loxi.out
+
+runscript:
+	make build
+	./loxi.out tmp.lox
